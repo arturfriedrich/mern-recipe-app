d6878e45a1deda8edcae21d7b56b020a75af8903
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-25 11:26:01 +0200

    user login added

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
 
 import { UserModel } from '../models/Users.js';
+
+dotenv.config()
 
 // Create a router
 const router = express.Router();
@@ -33,6 +36,24 @@ router.post("/register", async (req, res) => {
     res.json({ message: "User registered successfully" });
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+  
+    const user = await UserModel.findOne({ username });
+  
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User doesn't exists" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ message: "Username or password is incorrect" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ token, userID: user._id });
+  });
 
 export { router as userRouter }
