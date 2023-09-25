e5c845bff29a6e29b48979a3e530465e7443adc5
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-25 11:15:14 +0200

    Update Users.js

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,10 +1,8 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-// Creating a schema, sort of like working with an ORM
 const UserSchema = new mongoose.Schema({
-    username : { type: String, required: true, unique: true },
-    password : { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-// Creating a collection within database with the defined schema
-const UserModel = mongoose.model('users', UserSchema);
+export const UserModel = mongoose.model("users", UserSchema);
