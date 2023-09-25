70f9e55fc670635587302bb752262b10ab613ee5
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-25 15:30:30 +0200

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,15 +9,21 @@ export const Navbar = () => {
     const logout = () => {
         setCookies("access_token", "");
         window.localStorage.removeItem("userID");
-        navigate("/auth");
+        navigate("/");
     };
 
     return (
         <div className="navbar">
             <Link to="/">Home</Link>
             <Link to="/create-recipe">Create Recipe</Link>
-            <Link to="/saved-recipes">Saved Recipes</Link>
-            {!cookies.access_token ? (<Link to="/auth">Login/Register</Link>) : (<button onClick={() => logout()}>Logout</button>)}
+            {!cookies.access_token ? (
+                <Link to="/auth">Login/Register</Link>
+            ) : (
+                <>
+                    <Link to="/saved-recipes">Saved Recipes</Link>
+                    <button onClick={() => logout()}>Logout</button>
+                </>
+            )}
         </div>
     );
 };
