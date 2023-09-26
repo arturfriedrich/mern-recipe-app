9fd4d4f0d3e09e216f37d7ca901d017359052794
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-26 20:27:34 +0200

    redirect on register #6

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Form } from "../components/Form";
+import { useNavigate } from "react-router-dom";
 
 export const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -13,7 +16,10 @@ export const Register = () => {
                 username,
                 password
             })
+            
+            navigate("/auth");
             alert("User registered successfully");
+
         } catch(error) {
             console.error(error)
         }
