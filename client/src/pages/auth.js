85838b6419658251949d8957215e47cccffa1c85
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-26 15:13:37 +0200

    Update auth.js

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -12,9 +12,7 @@ export const Auth = ({ mode }) => {
                             <h1 className="card-title text-center">{ mode === "login" ? <>Login</> : <>Register</> }</h1>
                             { mode === "login" ? <Login /> : <Register /> }
                             <div className="text-center mt-3">
-                            { mode === "login" ? <Link className="link" to="/register">You don't have an account? Make one.</Link> : <Link className="link" to="/auth">You already have an account? Log in.</Link> }
-                                
-                                
+                            { mode === "login" ? <Link className="link" to="/register">You don't have an account? Make one.</Link> : <Link className="link" to="/auth">You already have an account? Log in.</Link> } 
                             </div>
                         </div>
                     </div>
