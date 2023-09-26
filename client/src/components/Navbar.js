cb301ad9aaf801a3ff089e3966f5d171d8a84225
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-26 10:28:22 +0200

    fix navbar hamburger menu #2

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,27 +13,48 @@ export const Navbar = () => {
     };
 
     return (
-        <nav className="navbar navbar-expand navbar-light bg-light">
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
             <Link className="navbar-brand" to="/">Home</Link>
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link className="nav-link" to="/create-recipe">Create Recipe</Link>
-                </li>
-                {!cookies.access_token ? (
+
+            {/* Hamburger Menu Toggle Button */}
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/auth">Login/Register</Link>
+                        <Link className="nav-link" to="/create-recipe">Create Recipe</Link>
                     </li>
-                ) : (
-                    <>
+                    {!cookies.access_token ? (
                         <li className="nav-item">
-                            <Link className="nav-link" to="/saved-recipes">Saved Recipes</Link>
+                            <Link className="btn btn-primary" to="/auth">Login</Link>
                         </li>
-                        <li className="nav-item d-flex justify-content-center align-items-center">
-                            <button className="btn btn-danger" onClick={() => logout()}>Logout</button>
-                        </li>
-                    </>
-                )}
-            </ul>
+                    ) : (
+                        <>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/saved-recipes">Saved Recipes</Link>
+                            </li>
+                            <li className="nav-item d-flex justify-content-center align-items-center">
+                                <button
+                                    className={`btn btn-danger ${!cookies.access_token ? 'd-none' : ''}`}
+                                    onClick={() => logout()}
+                                >
+                                    Logout
+                                </button>
+                            </li>
+                        </>
+                    )}
+                </ul>
+            </div>
         </nav>
     );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "jquery/dist/jquery.min.js";
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
