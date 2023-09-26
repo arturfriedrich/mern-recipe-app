43ff96b73f3473d91f0915bb9b65632cbccdd7aa
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-26 20:25:15 +0200

    no saved recipes text when empty #8

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -23,6 +23,6 @@ export const Auth = ({ mode }) => {
             </div>
           </div>
         </div>
-      </div>
+      </div>      
     );
   };
--- client/src/pages/saved-recipes.js
+++ client/src/pages/saved-recipes.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useGetUserID } from "../hooks/useGetUserID";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export const SavedRecipes = () => {
   const [savedRecipes, setSavedRecipes] = useState([]);
   const userID = useGetUserID();
+
+  console.log(savedRecipes)
 
   useEffect(() => {
     const fetchSavedRecipes = async () => {
@@ -21,19 +24,23 @@ export const SavedRecipes = () => {
     fetchSavedRecipes();
   }, []);
   return (
-    <div>
-      <h1>Saved Recipes</h1>
-      <ul>
-        {savedRecipes.map((recipe) => (
-          <li key={recipe._id}>
-            <div>
-              <h2>{recipe.name}</h2>
-            </div>
-            <p>{recipe.description}</p>
-            <img src={recipe.imageUrl} alt={recipe.name} />
-            <p>Cooking Time: {recipe.cookingTime} minutes</p>
-          </li>
-        ))}
+    <div className="saved-recipes-container">
+      <h1 className="saved-recipes-heading">Saved Recipes</h1>
+      <ul className="list-unstyled">
+        {savedRecipes.length === 0 ? (
+          <p className="no-saved-recipes">No saved recipes! Go to the<Link className="link" to="/">browse page</Link>to explore new recepies!</p>
+        ) : (
+          savedRecipes.map((recipe) => (
+            <li key={recipe._id} className="recipe-item">
+              <div>
+                <h2>{recipe.name}</h2>
+              </div>
+              <p>{recipe.description}</p>
+              <img src={recipe.imageUrl} alt={recipe.name} />
+              <p>Cooking Time: {recipe.cookingTime} minutes</p>
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
