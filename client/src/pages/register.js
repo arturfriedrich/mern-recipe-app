b646a106f1586138ef501ce7ce83b92e2fd1c69a
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-04 11:43:11 +0200

    better alert when register #13

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import Modal from "react-bootstrap/Modal"; // Import the modal component
+import Button from "react-bootstrap/Button"; // Import the button component
 
 import "../styles/login.css";
 
 export const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [showModal, setShowModal] = useState(false); // State to manage modal visibility
 
     const navigate = useNavigate();
 
@@ -16,14 +19,18 @@ export const Register = () => {
             await axios.post("http://localhost:4000/auth/register", {
                 username,
                 password
-            })
-            
-            navigate("/login");
-            alert("User registered successfully");
+            });
 
+            // Show the modal after successful registration
+            setShowModal(true);
         } catch(error) {
-            console.error(error)
+            console.error(error);
         }
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        navigate("/login");
     };
 
     return (
@@ -62,6 +69,20 @@ export const Register = () => {
             <div className="col-md-6">
                 <img src={require("../assets/undraw_barbecue_3x93.svg").default} alt="login" />
             </div>
+
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Registration Successful</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    You have registered successfully. Now you can<Link className="login-link" to="/login">Log in</Link>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 };
